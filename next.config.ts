83e951b49702f2513fe9b6bb7a5b450e212e8f85
Jeiel0rbit <jeiel.lima.miranda@gmail.com>
2025-08-01 12:00:58 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // 1. Diga ao Next.js para criar um site estático, que funciona melhor no GitHub Pages.
+  output: 'export',
+
+  // 2. Diga ao Next.js que o site está dentro da pasta "/portfolio".
+  basePath: "/portfolio", 
+  assetPrefix: "/portfolio/", 
+
   images: {
+    // 3. Desative a otimização de imagens do Next.js, pois ela não funciona bem com sites estáticos.
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
